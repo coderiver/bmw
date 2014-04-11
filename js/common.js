9f5207ca4e669935600abb2efcc3ac4f7cb76f11
hraboviyvadim <hraboviyvadim@gmail.com>
2014-04-11 16:54:13 +0300

    header nav dropdown

--- js/common.js
+++ js/common.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
 
+	// main-menu dropdown
+	
+	$('.mainmenu__dropdown').hide();
+
+	$(".showroom").on('click', function(){
+		$(this).toggleClass('is-active');
+		$(".mainmenu").toggleClass('drop');
+		$(this).parent().find('.mainmenu__dropdown').toggle();
+	});
+
+	$(".showroom").hover(
+		function(){
+			if ($(this).parents('.mainmenu').hasClass('drop')) {
+				$(".showroom").removeClass('is-active');	
+				$(this).addClass('is-active');
+				$(".showroom").parent().find('.mainmenu__dropdown').hide();
+				$(this).parent().find('.mainmenu__dropdown').show();
+			};
+		},
+		function(){
+			//$(this).removeClass('is-active');
+		}
+	);
+
 });
